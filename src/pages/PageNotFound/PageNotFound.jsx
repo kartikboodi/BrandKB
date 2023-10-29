4f46e3abe63bd85dbaddaaaeab7a3d735932bc37
Kartik Boodi <96716653+kartikboodi@users.noreply.github.com>
2023-10-30 02:18:11 +0530

    Pipeline issues fix (#12)

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,22 +1,20 @@
 import Header from "../../components/Header";
 import { HeaderRanks } from "../../components/Header/HeaderConstants";
 import Layout from "../../components/Layout";
-import lostEmoji from "../../data/LostEmoji.svg";
+import lostEmoji from "../../data/sadFaceEmoji.svg";
 
 import "./PageNotFound.css";
 
 const PageNotFound = () => {
   return (
     <div className="PageNotFound">
-      <Layout>
-        <div className="ErrorCode">
-          <span>4</span>
-          <img className="lostEmoji" src={lostEmoji} alt="lost emoji" />
-          <span>4</span>
-        </div>
-        <span className="Oops">Oops!</span>
-        <Header rank={HeaderRanks.FIRST}>Looks like you're lost</Header>
-      </Layout>
+      <div className="ErrorCode">
+        <span>4</span>
+        <img className="lostEmoji" src={lostEmoji} alt="lost emoji" />
+        <span>4</span>
+      </div>
+      <span className="Oops">Oops!</span>
+      <Header rank={HeaderRanks.FIRST}>Looks like you're lost</Header>
     </div>
   );
 };
