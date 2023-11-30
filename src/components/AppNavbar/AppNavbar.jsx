e027f8c62addbd8de796087816cc4323aa24d232
Kartik Boodi <96716653+kartikboodi@users.noreply.github.com>
2023-11-30 23:43:52 +0530

    import issue fixed (#15)

--- src/components/AppNavbar/AppNavbar.jsx
+++ src/components/AppNavbar/AppNavbar.jsx
@@ -7,9 +7,9 @@ import logo from "../../data/KBLogo.svg";
 import Header from "../Header";
 import { HeaderRanks } from "../Header/HeaderConstants";
 import { PATHS } from "../Router/constants";
+import { NAVBAR_TITLES } from "../../data/constants";
 
 import "./AppNavbar.css";
-import { NAVBAR_TITLES } from "../../data/constants";
 
 const AppNavbar = () => {
   return (
