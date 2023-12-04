fccb47b7e7ffc584106d2c858f0d85e349aeefa0
kartikboodi <kartikboodi@gmail.com>
2023-12-05 00:56:01 +0530

    FE skills

---
+++ src/pages/Skills/components/BackEnd.jsx
@@ -0,0 +1,10 @@
+import SkillsList from "./SkillsList/SkillsList";
+
+const BackEnd = () => {
+  const title = "Back-end Development";
+  const listItems = [];
+
+  return <SkillsList title={title} listItems={listItems} />;
+};
+
+export default BackEnd;
