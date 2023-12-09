2266ad427901ec7f3ff49b245d64773815564775
kartikboodi <kartikboodi@gmail.com>
2023-12-09 23:31:57 +0530

    click sound to tab change

--- src/components/VerticalTabs/VerticalTabs.jsx
+++ src/components/VerticalTabs/VerticalTabs.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Nav, Row, Tab } from "react-bootstrap";
+import clickAudio from "../../data/pen-click.wav";
 
 import "./VerticalTabs.css";
 
 const VerticalTabs = ({ tabs }) => {
   const tabTitles = tabs ? Object.keys(tabs) : [];
+  const audio = new Audio(clickAudio);
+
+  const [activeTab, setActiveTab] = useState(1);
+
+  const handleTabClick = (eventKey) => {
+    if (eventKey !== activeTab) {
+      audio.play();
+      setActiveTab(eventKey);
+    }
+  };
+
   return (
-    <Tab.Container defaultActiveKey={1}>
+    <Tab.Container defaultActiveKey={1} activeKey={activeTab}>
       <Row className="vertical-tabs-row">
         <Col sm={2}>
           <Nav variant="tabs" className="vertical-tabs">
             {tabTitles?.map((tabTitle) => (
               <Nav.Item>
-                <Nav.Link eventKey={tabTitles.indexOf(tabTitle) + 1}>
+                <Nav.Link
+                  eventKey={tabTitles.indexOf(tabTitle) + 1}
+                  onClick={() =>
+                    handleTabClick(tabTitles.indexOf(tabTitle) + 1)
+                  }
+                >
                   {tabTitle}
                 </Nav.Link>
               </Nav.Item>
--- src/pages/Skills/components/Tools.jsx
+++ src/pages/Skills/components/Tools.jsx
@@ -8,3 +8,4 @@ const Tools = () => {
 };
 
 export default Tools;
+ 
